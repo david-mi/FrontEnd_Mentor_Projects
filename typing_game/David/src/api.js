3d0e2943b1560f59635067b062b03bbe4f4b1b25
David Michel <david.michel2809@gmail.com>
2022-10-23 15:06:32 +0200

    feat: adding https to sentence api

--- typing_game/David/src/api.js
+++ typing_game/David/src/api.js
@@ -1,5 +1,5 @@
 export const getApiSentence = async () => {
-  const response = await fetch("http://api.quotable.io/random");
+  const response = await fetch("https://api.quotable.io/random");
   const data = await response.json();
   return data.content;
 };
