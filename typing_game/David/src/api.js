5c3bf4e90101fb96beffe59bbd0e063acbad889d
David Michel <david.michel2809@gmail.com>
2022-10-23 16:49:07 +0200

    feat: setting minimum quote length

--- typing_game/David/src/api.js
+++ typing_game/David/src/api.js
@@ -1,5 +1,5 @@
 export const getApiSentence = async () => {
-  const response = await fetch("https://api.quotable.io/random");
+  const response = await fetch("https://api.quotable.io/random?minLength=150");
   const data = await response.json();
   return data.content;
 };
